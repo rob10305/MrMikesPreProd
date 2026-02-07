ed62875039504de040f05fc0df2ed6afe0caffbf
rob10305 <rob@mudu.ca>
2026-02-06 20:49:52 -0500

    Update gallery data - 2/6/2026, 8:49:52 PM

--- gallery-data.js
+++ gallery-data.js
@@ -2,5 +2,31 @@
  * MrMikes Gallery Data
  * This file is automatically updated when gallery images are added/edited via the admin panel.
  * DO NOT EDIT MANUALLY - changes will be overwritten.
+ * Last updated: 2026-02-07T01:49:51.946Z
  */
-const GALLERY_DATA = [];
+const GALLERY_DATA = [
+  {
+    "id": "1770417412110",
+    "image": "images/1770417409913_537101457_10161903633305868_9173619720149172889_n.jpg",
+    "caption": "A test for the gallery page.",
+    "category": "triumph"
+  },
+  {
+    "id": "1770417532962",
+    "image": "images/1770419491543_WhatsApp_Image_2025-09-16_at_07.29.11_5524cce9.jpg",
+    "caption": "My White TR6",
+    "category": "triumph"
+  },
+  {
+    "id": "1770418125505",
+    "image": "images/1770418118264_green_TR6.png",
+    "caption": "Its kermits car!",
+    "category": "triumph"
+  },
+  {
+    "id": "1770418182343",
+    "image": "images/1770418177406_LarryLambert-BI.jpg",
+    "caption": "An MG test post for Gallery",
+    "category": "mg"
+  }
+];
