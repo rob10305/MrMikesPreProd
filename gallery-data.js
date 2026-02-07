8953546a48f4f3a28518ee5ddf57002a8a15d6c0
rob10305 <rob@mudu.ca>
2026-02-06 20:49:59 -0500

    Update gallery data - 2/6/2026, 8:49:58 PM

--- gallery-data.js
+++ gallery-data.js
@@ -2,7 +2,7 @@
  * MrMikes Gallery Data
  * This file is automatically updated when gallery images are added/edited via the admin panel.
  * DO NOT EDIT MANUALLY - changes will be overwritten.
- * Last updated: 2026-02-07T01:49:51.946Z
+ * Last updated: 2026-02-07T01:49:58.574Z
  */
 const GALLERY_DATA = [
   {
@@ -14,7 +14,7 @@ const GALLERY_DATA = [
   {
     "id": "1770417532962",
     "image": "images/1770419491543_WhatsApp_Image_2025-09-16_at_07.29.11_5524cce9.jpg",
-    "caption": "My White TR6",
+    "caption": "My White TR6!",
     "category": "triumph"
   },
   {
